ba4978de4677c29e3f02234165d1b03f6c332b14
nguyenhuuduc458 <45595762+nguyenhuuduc458@users.noreply.github.com>
2019-12-06 17:10:32 +0700

    thay đổi view báo cáo ngày 6/12/2019

---
+++ Project2/src/ApplicationCore/Services/NhanVienService.cs
@@ -0,0 +1,256 @@
+using System.Dynamic;
+using System.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography;
+using System.Text;
+using ApplicationCore.DTOs;
+using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using ApplicationCore.Interfaces.IServices;
+using AutoMapper;
+using ApplicationCore.Specifications;
+
+namespace ApplicationCore.Services
+{
+    public class NhanVienService : INhanVienService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        public NhanVienService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public void Create(SaveNhanVienDTO nhanvien)
+        {
+            var nv = _mapper.Map<SaveNhanVienDTO, NhanVien>(nhanvien);
+            MD5 md5hash = MD5.Create();
+            if(nv.MaVaiTro == 1){
+                NhanVien nv2 = new NhanVien
+                {
+                    HoTen = nv.HoTen,
+                    NgaySinh = nv.NgaySinh,
+                    DiaChi = nv.DiaChi,
+                    MaVaiTro = nv.MaVaiTro,
+                    GioiTinh = nv.GioiTinh,
+                    TenDangNhap = "Admin" + getMaTaiKhoan(),
+                    MatKhau = GetMd5hash(md5hash, "123")
+                };
+                _unitOfWork.NhanViens.Add(nv2);
+
+            }else if(nv.MaVaiTro == 2){
+                NhanVien nv2 = new NhanVien
+                {
+                    HoTen = nv.HoTen,
+                    NgaySinh = nv.NgaySinh,
+                    DiaChi = nv.DiaChi,
+                    MaVaiTro = nv.MaVaiTro,
+                    GioiTinh = nv.GioiTinh,
+                    TenDangNhap = "bacsi" + getMaTaiKhoan(),
+                    MatKhau = GetMd5hash(md5hash, "123")
+                };
+                _unitOfWork.NhanViens.Add(nv2);
+
+            }else{
+                NhanVien nv2 = new NhanVien
+                {
+                    HoTen = nv.HoTen,
+                    NgaySinh = nv.NgaySinh,
+                    DiaChi = nv.DiaChi,
+                    MaVaiTro = nv.MaVaiTro,
+                    GioiTinh = nv.GioiTinh,
+                    TenDangNhap = "nhanvien" + getMaTaiKhoan(),
+                    MatKhau = GetMd5hash(md5hash, "123")
+                };
+                _unitOfWork.NhanViens.Add(nv2);
+            }
+            _unitOfWork.Complete();
+        }
+
+        public void Delete(int id)
+        {
+            var nv = _unitOfWork.NhanViens.GetById(id);
+            _unitOfWork.NhanViens.Remove(nv);
+            _unitOfWork.Complete();
+        }
+
+        public void Edit(SaveNhanVienDTO nhanvien)
+        {
+            var nv = _mapper.Map<SaveNhanVienDTO, NhanVien>(nhanvien);
+            _unitOfWork.NhanViens.Update(nv);
+            _unitOfWork.Complete();
+        }
+
+        public IEnumerable<SaveVaiTroDTO> GetAllMaVT()
+        {
+            IEnumerable<VaiTro> vt = _unitOfWork.VaiTros.GetAll();
+            return _mapper.Map<IEnumerable<VaiTro>, IEnumerable<SaveVaiTroDTO>>(vt);
+        }
+
+        public IEnumerable<SaveVaiTroDTO> GetDSVTConlai(int MaVaiTro)
+        {
+            Expression<Func<VaiTro, bool>> predicate = m => true;
+
+            predicate = m => m.MaVaiTro != MaVaiTro;
+
+            IEnumerable<VaiTro> vt = _unitOfWork.VaiTros.Find(predicate);
+            return _mapper.Map<IEnumerable<VaiTro>, IEnumerable<SaveVaiTroDTO>>(vt);
+        }
+
+        public SaveNhanVienDTO GetNhanVien(int id)
+        {
+            var nhanvien = _unitOfWork.NhanViens.GetById(id);
+            return _mapper.Map<NhanVien, SaveNhanVienDTO>(nhanvien);
+        }
+
+        public IEnumerable<SaveNhanVienDTO> GetNhanViens(string sortOrder, string searchString, int pageIndex, int pageSize, out int count)
+        {
+            NhanVienSpecification nhanVienFilterPaging = new NhanVienSpecification(searchString, pageIndex, pageSize);
+            NhanVienSpecification nhanVienFilter = new NhanVienSpecification(searchString);
+             
+            var NhanVien = _unitOfWork.NhanViens.FindSpec(nhanVienFilterPaging);
+            count = _unitOfWork.NhanViens.Count(nhanVienFilter);
+            //sort 
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    NhanVien = NhanVien.OrderByDescending(m => m.HoTen);
+                    break;
+                case "Date":
+                    NhanVien = NhanVien.OrderBy(m => m.NgaySinh);
+                    break;
+                case "Date_desc":
+                    NhanVien = NhanVien.OrderByDescending(m => m.NgaySinh);
+                    break;
+                default:
+                    NhanVien = NhanVien.OrderBy(m => m.DiaChi);
+                    break;
+            }
+            return _mapper.Map<IEnumerable<NhanVien>, IEnumerable<SaveNhanVienDTO>>(NhanVien);
+        }
+
+        public string GetTenVaiTro(int MaVaiTro)
+        {
+            Expression<Func<VaiTro, bool>> predicate = m => true;
+            predicate = m => m.MaVaiTro == MaVaiTro;
+            return _unitOfWork.VaiTros.Find(predicate).Select(m => m.TenVaiTro).FirstOrDefault().ToString();
+        }
+
+        public bool Login(string username, string password)
+        {
+            // mã hóa password trước khi tìm kiếm 
+            MD5 md5 = MD5.Create();
+            var hasPassword = GetMd5hash(md5, password);
+
+            FindNhanVienSpecification nhanVienSpecification = new FindNhanVienSpecification(username,hasPassword);
+            var nhanVien = _unitOfWork.NhanViens.FindSpec(nhanVienSpecification).FirstOrDefault();
+            
+            string usernamesql;
+            string passwordsql;
+            if(nhanVien == null){
+                return false;
+            }else{
+                usernamesql = nhanVien.TenDangNhap;
+                passwordsql = nhanVien.MatKhau;
+            }
+               
+              
+            using (MD5 md5hash = MD5.Create())
+            {
+                if (VerifyMd5Hash(md5hash, password, passwordsql) && usernamesql == username)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        
+        public string GetMd5hash(MD5 md5hash, string input)
+        {
+            byte[] data = md5hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sb.Append(data[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+        public bool VerifyMd5Hash(MD5 md5hash, string input, string hash)
+        {
+            var hashOfInput = GetMd5hash(md5hash, input);
+            if (String.Compare(hash, hashOfInput) == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public List<string> CreateSession(string username,string password)
+        {   
+            // mã hóa password trước khi tìm kiếm
+            MD5 md5hash = MD5.Create();
+            var hasPassword = GetMd5hash(md5hash, password); 
+
+            List<string> listSession = new List<string>();
+
+            FindNhanVienSpecification nhanVienSpecification = new FindNhanVienSpecification(username, hasPassword);
+            var nhanVien = _unitOfWork.NhanViens.FindSpec(nhanVienSpecification).FirstOrDefault();
+
+            string tennhanvien = nhanVien.HoTen;
+            int role           = nhanVien.MaVaiTro;
+            int manhanvien     = nhanVien.MaNhanVien;
+            
+            listSession.Add(tennhanvien);
+            listSession.Add(role.ToString());
+            listSession.Add(manhanvien.ToString());
+            return listSession;
+        }
+        // tao tên đăng nhập mac dinh cho tài khoản
+        public int getMaTaiKhoan(){
+            
+            var ListNhanVien = _unitOfWork.NhanViens.GetAll();
+
+            var nhanvien = ListNhanVien.ElementAtOrDefault(0);
+
+            var max = nhanvien.MaNhanVien;
+
+            foreach(var nv in ListNhanVien){
+                if(nv.MaNhanVien >= max){
+                    max = nv.MaNhanVien;
+                }
+            }
+            return max;
+        }
+       
+        public void UpdatePassword(string username,string matKhau,string MatKhauMoi)
+        {
+            using (MD5 md5hash = MD5.Create())
+            {
+                var hashPassword = GetMd5hash(md5hash, matKhau);
+                var hashPasswordNew = GetMd5hash(md5hash, MatKhauMoi);
+                _unitOfWork.NhanViens.UpdatePassword(username,hashPassword,hashPasswordNew);
+            }
+        }
+
+        public bool checkValidPassword(string username, string matKhau)
+        {
+            using(MD5 md5hash = MD5.Create()){
+                var hashPassword = GetMd5hash(md5hash, matKhau);
+                FindNhanVienSpecification nv = new FindNhanVienSpecification(username,hashPassword);
+                NhanVien nhanvien = _unitOfWork.NhanViens.FindSpec(nv).FirstOrDefault();
+                if (nhanvien == null)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+    }
+}
---
+++ Project2/src/MVCPage/Controllers/NhanVienController.cs
@@ -0,0 +1,261 @@
+using System.Net.Http;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+using ApplicationCore.DTOs;
+using ApplicationCore.Entities;
+using ApplicationCore.Interfaces.IServices;
+using AutoMapper;
+using Infrastructure.Persistence;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MVCPage.Services;
+using MVCPage.ViewModel;
+
+namespace MVCPage.Controllers {
+    public class NhanVienController : Controller
+    {
+        private readonly INhanVienServiceView _serviceView;
+        private readonly INhanVienService _service;
+        private readonly IMapper _mapper;
+
+        public NhanVienController(INhanVienServiceView serviceView, INhanVienService service, IMapper mapper)
+        {
+            _serviceView = serviceView;
+            _service = service;
+            _mapper = mapper;
+
+        }
+        // GET: NhanVien
+        public IActionResult Index(string sortOrder, string CurrentFilter, int pageIndex = 1)
+        {
+            if (HttpContext.Session.GetString("Username") != null && HttpContext.Session.GetString("Role") == "1")
+            {
+                ViewData["NameSortParam"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+                ViewData["DateSortParam"] = sortOrder == "Date" ? "Date_desc" : "Date";
+                ViewData["CurrentFilter"] = CurrentFilter;
+                ViewData["CurrentSort"] = sortOrder;
+                NhanVienIndexVM indexNV = _serviceView.GetNhanVienIndexVM(sortOrder, CurrentFilter, pageIndex);
+                return View(indexNV);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Account");
+            }
+        }
+
+        // GET: NhanVien/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (HttpContext.Session.GetString("Username") != null && HttpContext.Session.GetString("Role") == "1")
+            {
+                if (id == null)
+                {
+                    return NotFound();
+                }
+
+                var nhanVien = _service.GetNhanVien(id.Value);
+                if (nhanVien == null)
+                {
+                    return NotFound();
+                }
+                return View(nhanVien);
+
+            }
+            else
+            {
+                return RedirectToAction("Index", "Account");
+            }
+        }
+
+        // GET: NhanVien/Create
+
+        public IActionResult Create()
+        {
+            if (HttpContext.Session.GetString("Username") != null && HttpContext.Session.GetString("Role") == "1")
+            {
+                ViewData["MaVaiTro"] = _service.GetAllMaVT();
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Account");
+            }
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("MaVaiTro,HoTen,GioiTinh,NgaySinh,DiaChi")] SaveNhanVienDTO nhanVien)
+        {
+            if (HttpContext.Session.GetString("Username") != null && HttpContext.Session.GetString("Role") == "1")
+            {
+                _service.Create(nhanVien);
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return RedirectToAction("Index", "Account");
+            }
+
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (HttpContext.Session.GetString("Username") != null && HttpContext.Session.GetString("Role") == "1")
+            {
+                if (id == null)
+                {
+                    return NotFound();
+                }
+
+                var nhanVien = _service.GetNhanVien(id.Value);
+                if (nhanVien == null)
+                {
+                    return NotFound();
+                }
+                ViewData["MaVaiTro"] = nhanVien.MaVaiTro.ToString();
+                ViewData["TenVaiTro"] = _service.GetTenVaiTro(nhanVien.MaVaiTro);
+                ViewData["DanhSach"] = _service.GetDSVTConlai(nhanVien.MaVaiTro);
+                ViewData["GioiTinh"] = nhanVien.GioiTinh.ToString();
+                ViewData["GioiTinhConLai"] = nhanVien.GioiTinh.ToString().Equals("Nam") ? "Nữ" : "Nam";
+                return View(nhanVien);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Account");
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, [Bind("MaNhanVien,MaVaiTro,HoTen,GioiTinh,NgaySinh,DiaChi,TenDangNhap,MatKhau")] SaveNhanVienDTO nhanVien)
+        {
+            if (HttpContext.Session.GetString("Username") != null && HttpContext.Session.GetString("Role") == "1")
+            {
+                if (id != nhanVien.MaNhanVien)
+                {
+                    return NotFound();
+                }
+                _service.Edit(nhanVien);
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return RedirectToAction("Index", "Account");
+            }
+
+        }
+
+        // GET: NhanVien/Delete/5
+        public IActionResult Delete(int? id)
+        {
+            if (HttpContext.Session.GetString("Username") != null && HttpContext.Session.GetString("Role") == "1")
+            {
+                if (id == null)
+                {
+                    return NotFound();
+                }
+
+                var nhanVien = _service.GetNhanVien(id.Value);
+                if (nhanVien == null)
+                {
+                    return NotFound();
+                }
+                return View(nhanVien);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Account");
+            }
+        }
+
+        // POST: NhanVien/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            if (HttpContext.Session.GetString("Username") != null && HttpContext.Session.GetString("Role") == "1")
+            {
+                _service.Delete(id);
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return RedirectToAction("Index", "Account");
+            }
+        }
+
+        public IActionResult DetailsPersonal()
+        {
+            if (HttpContext.Session.GetString("Username") != null)
+            {
+                var MaNhanVien = HttpContext.Session.GetString("MaNhanVien");
+                var nhanVien = _service.GetNhanVien(Convert.ToInt32(MaNhanVien));
+                ViewData["MaVaiTro"] = nhanVien.MaVaiTro.ToString();
+                ViewData["TenVaiTro"] = _service.GetTenVaiTro(nhanVien.MaVaiTro);
+                ViewData["DanhSach"] = _service.GetDSVTConlai(nhanVien.MaVaiTro);
+                ViewData["GioiTinh"] = nhanVien.GioiTinh.ToString();
+                ViewData["GioiTinhConLai"] = nhanVien.GioiTinh.ToString().Equals("Nam") ? "Nữ" : "Nam";
+                return View(nhanVien);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Account");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult DetailsPersonal(SaveNhanVienDTO nhanVien)
+        {
+            if (HttpContext.Session.GetString("Username") != null)
+            {
+                _service.Edit(nhanVien);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Account");
+            }
+        }
+
+        public IActionResult ChangePassword()
+        {
+            if (HttpContext.Session.GetString("Username") != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Account");
+            }
+        }
+        [HttpPost]
+        public IActionResult ChangePassword(string MatKhau, string pass2)
+        {
+            if (HttpContext.Session.GetString("Username") != null)
+            {
+                if (_service.checkValidPassword(HttpContext.Session.GetString("Username"), MatKhau))
+                {
+                    _service.UpdatePassword(HttpContext.Session.GetString("Username"), MatKhau, pass2);
+                }
+                else
+                {
+                    ViewData["InvalidPassword"] = "Mật khẩu cũ không khớp";
+                    ViewData["MatKhau"] = MatKhau;
+                    return View();
+                }
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Account");
+            }
+        }
+    }
+}
